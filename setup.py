31e56bbf507f720aa808bef0fdd930189451356c
Adam Sampson <ats@offog.org>
2003-05-03 12:42:04 +0000

    Initial revision

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "rawdog",
+	version = "0.2",
+	description = "RSS Aggregator Without Delusions Of Grandeur",
+	author = "Adam Sampson",
+	author_email = "azz@us-lot.org",
+	url = "http://azz.us-lot.org/code.html",
+	license = "GNU GPL v2 or later",
+	scripts = ['rawdog'],
+	packages = ['rawdoglib'])
+
+

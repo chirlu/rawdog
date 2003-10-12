90519cbda8ba4e9315ad85165e5669bcec683214
Adam Sampson <ats@offog.org>
2003-10-12 13:29:41 +0000

    Update version.

--- rawdoglib/rawdog.py
+++ rawdoglib/rawdog.py
@@ -16,7 +16,7 @@
 # Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA 02111-1307 USA, or see http://www.gnu.org/.
 
-VERSION = "1.4"
+VERSION = "1.5"
 import feedparser
 from persister import Persistable, Persister
 import os, time, sha, getopt, sys

e7699835cbba1bf1a871129d4cf6f810ea32f30f
Adam Sampson <ats@offog.org>
2003-11-18 18:11:24 +0000

    Version 1.7.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name = "rawdog",
-	version = "1.6",
+	version = "1.7",
 	description = "RSS Aggregator Without Delusions Of Grandeur",
 	author = "Adam Sampson",
 	author_email = "azz@us-lot.org",

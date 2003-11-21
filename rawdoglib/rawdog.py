f3ac632656f09f383bf9eb3db04f8fa7dca42b39
Adam Sampson <ats@offog.org>
2003-11-21 17:30:08 +0000

    Version 1.8.

--- rawdoglib/rawdog.py
+++ rawdoglib/rawdog.py
@@ -16,7 +16,7 @@
 # Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA 02111-1307 USA, or see http://www.gnu.org/.
 
-VERSION = "1.7"
+VERSION = "1.8"
 import feedparser, iso8601
 from persister import Persistable, Persister
 import os, time, sha, getopt, sys, re, urlparse

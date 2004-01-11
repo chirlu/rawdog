4d7f2e543f9b03f97bd677c37db20444369ef101
Adam Sampson <ats@offog.org>
2004-01-11 16:18:39 +0000

    Relicense persister under the LGPL.

--- rawdoglib/persister.py
+++ rawdoglib/persister.py
@@ -1,20 +1,20 @@
-# rawdog: RSS aggregator without delusions of grandeur.
-# Copyright 2003 Adam Sampson <azz@us-lot.org>
+# persister: safe class persistance wrapper
+# Copyright 2003, 2004 Adam Sampson <azz@us-lot.org>
 #
-# rawdog is free software; you can redistribute and/or modify it
-# under the terms of that license as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option)
-# any later version.
+# persister is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1 of the
+# License, or (at your option) any later version.
 #
-# rawdog is distributed in the hope that it will be useful, but
+# persister is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with rawdog; see the file COPYING. If not, write to the
-# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-# MA 02111-1307 USA, or see http://www.gnu.org/.
+# You should have received a copy of the GNU Lesser General Public
+# License along with persister; see the file COPYING.LGPL. If not,
+# write to the Free Software Foundation, Inc., 59 Temple Place, Suite
+# 330, Boston, MA 02111-1307 USA, or see http://www.gnu.org/.
 
 import pickle, fcntl
 

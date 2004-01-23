7c4ebffda5bbb52ecbe0720b979a9b0bd44ed4fb
Adam Sampson <ats@offog.org>
2004-01-23 00:55:43 +0000

    Update version to 1.9rc2.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name = "rawdog",
-	version = "1.9rc1",
+	version = "1.9rc2",
 	description = "RSS Aggregator Without Delusions Of Grandeur",
 	author = "Adam Sampson",
 	author_email = "azz@us-lot.org",

1be3a1d4f51ab9d4b3423b7e4ce04615f5b708e6
Adam Sampson <ats@offog.org>
2005-01-04 23:29:01 +0000

    rawdog 2.2rc1.

--- rawdoglib/rawdog.py
+++ rawdoglib/rawdog.py
@@ -16,7 +16,7 @@
 # Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA 02111-1307 USA, or see http://www.gnu.org/.
 
-VERSION = "2.2"
+VERSION = "2.2rc1"
 STATE_VERSION = 2
 import feedparser, feedfinder, plugins
 from persister import Persistable, Persister

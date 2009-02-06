4370248f920c4c79b9e4abf3b98d6d537e83c726
Adam Sampson <ats@offog.org>
2009-02-06 17:42:14 +0000

    Version 2.12.

--- rawdoglib/rawdog.py
+++ rawdoglib/rawdog.py
@@ -16,7 +16,7 @@
 # Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA, or see http://www.gnu.org/.
 
-VERSION = "2.12rc3"
+VERSION = "2.12"
 STATE_VERSION = 2
 import feedparser, plugins
 from persister import Persistable, Persister

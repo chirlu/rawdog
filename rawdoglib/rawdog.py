dff8fdfc5a3b38a1b7931c461e4be91ec0712382
Adam Sampson <ats@offog.org>
2010-06-26 09:57:11 +0000

    Work around broken DOCTYPEs that confuse sgmllib.

--- rawdoglib/rawdog.py
+++ rawdoglib/rawdog.py
@@ -110,6 +110,11 @@ def sanitise_html(html, baseurl, inline, config):
 	# sgmllib handles "<br/>/" as a SHORTTAG; this workaround from
 	# feedparser.
 	html = re.sub(r'(\S)/>', r'\1 />', html)
+
+	# sgmllib is fragile with broken processing instructions (e.g.
+	# "<!doctype html!>"); just remove them all.
+	html = re.sub(r'<![^>]*>', '', html)
+
 	html = feedparser._resolveRelativeURIs(html, baseurl, "UTF-8", type)
 	p = feedparser._HTMLSanitizer("UTF-8", type)
 	p.feed(html)

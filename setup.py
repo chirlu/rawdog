556a2ca097853d5018005adc56042f4cb054d4d9
Adam Sampson <ats@offog.org>
2013-07-15 21:11:37 +0000

    rawdog 2.16rc1.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info < (2, 6) or sys.version_info >= (3,):
 	sys.exit(1)
 
 setup(name = "rawdog",
-	version = "2.15",
+	version = "2.16rc1",
 	description = "RSS Aggregator Without Delusions Of Grandeur",
 	author = "Adam Sampson",
 	author_email = "ats@offog.org",

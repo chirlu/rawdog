6270f2401052020cace989c0bae322262e444acd
Adam Sampson <ats@offog.org>
2015-07-19 00:08:57 +0100

    rawdog 2.21.

--- rawdoglib/rawdog.py
+++ rawdoglib/rawdog.py
@@ -16,7 +16,7 @@
 # Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA, or see http://www.gnu.org/.
 
-VERSION = "2.21rc1"
+VERSION = "2.21"
 HTTP_AGENT = "rawdog/" + VERSION
 STATE_VERSION = 2
 

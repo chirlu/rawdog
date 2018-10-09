97e39ed7b4a8577eae3a6f423a653859aa4a98a8
Adam Sampson <ats@offog.org>
2018-10-09 20:24:25 +0100

    Update COPYING and license notices to current versions.
    rawdog is still GPL v2+, but the FSF's current text has some minor
    formatting improvements, and the license notice is clearer.

--- rawdoglib/rawdog.py
+++ rawdoglib/rawdog.py
@@ -1,20 +1,18 @@
 # rawdog: RSS aggregator without delusions of grandeur.
 # Copyright 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2012, 2013, 2014, 2015, 2016, 2017, 2018 Adam Sampson <ats@offog.org>
 #
-# rawdog is free software; you can redistribute and/or modify it
-# under the terms of that license as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option)
-# any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 #
-# rawdog is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with rawdog; see the file COPYING. If not, write to the Free
-# Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-# MA 02110-1301, USA, or see http://www.gnu.org/.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 VERSION = "2.23rc1"
 HTTP_AGENT = "rawdog/" + VERSION

62a8fd141f56d62010c0828bb30cf16e3a4bf03f
Adam Sampson <ats@offog.org>
2019-10-07 15:05:25 +0100

    rawdog 2.24rc1.

--- rawdoglib/rawdog.py
+++ rawdoglib/rawdog.py
@@ -1,5 +1,5 @@
 # rawdog: RSS aggregator without delusions of grandeur.
-# Copyright 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2012, 2013, 2014, 2015, 2016, 2017, 2018 Adam Sampson <ats@offog.org>
+# Copyright 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019 Adam Sampson <ats@offog.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-VERSION = "2.23"
+VERSION = "2.24rc1"
 HTTP_AGENT = "rawdog/" + VERSION
 STATE_VERSION = 2
 
